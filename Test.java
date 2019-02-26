b0f2d058c7cb9b5ee6036ee3212d7406d0ccd453
Lesley Lai <lesleylai@lesleylai.info>
2019-02-25 22:46:44 -0700

    Init commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        String hello = "Hello"; // Name shadowing
+        int x = 1; // Unused variable
+        System.out.println(String.format("%s, %s!!",
+                                         hello,
+                                         iAmAReallyLongAndUglyJavaIdentifier));
+    }
+
+    private static String hello = "Real hello";
+    private static String iAmAReallyLongAndUglyJavaIdentifier = "world";
+}
